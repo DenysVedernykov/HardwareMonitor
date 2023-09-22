37ab6b74d591def46c22706b7b669c28dd63299b
Denys Vedernykov <razom.info@gmail.com>
2023-09-22 16:14:41 +0300

    Initial commit

---
+++ HardwareMonitor/Program.cs
@@ -0,0 +1,38 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+var _computer = new Computer()
+{
+    IsCpuEnabled = true,
+    IsMemoryEnabled = true,
+    IsStorageEnabled = true,
+};
+
+_computer.Open();
+
+foreach (var hardware in _computer.Hardware)
+{
+    hardware.Update();
+
+    foreach (var sensor in hardware.Sensors)
+    {
+        if (sensor.SensorType == SensorType.Load)
+        {
+            if (sensor.Name.Contains("CPU Total"))
+            {
+                Console.WriteLine($"{sensor.Name} {sensor.Value.GetValueOrDefault()}");
+            }
+            else if (sensor.Name.Contains("Virtual Memory"))
+            {
+                Console.WriteLine($"{sensor.Name} {sensor.Value.GetValueOrDefault()}");
+            }
+            else if (sensor.Name.Contains("Memory"))
+            {
+                Console.WriteLine($"{sensor.Name} {sensor.Value.GetValueOrDefault()}");
+            }
+        }
+    }
+}
+
+_computer.Close();
+
+Console.ReadKey();
